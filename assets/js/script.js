0cad00ca3d113fbf03a1ffaeb8ca802d9c0baa2a
Stefan Dimitrov <stefan@dimitrov.li>
2016-08-08 04:18:07 +0000

    First commit.

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    // -------------------------------------------------------------
+    // Countdown
+    // -------------------------------------------------------------
+    (function () {
+
+        $("#countdowndate").countdown({
+            date: "1 september 2016 12:00:00",
+            format: "on"
+        });
+
+    }());
+
+
+}); // JQuery end
